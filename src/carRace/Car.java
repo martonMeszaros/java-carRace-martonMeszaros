154be1f8b8fa94edace8f775b1e751fe7163f1a8
martonMeszaros <marton.m95@gmail.com>
2017-08-16 17:49:49 +0200

    Implemented the Car class

--- src/carRace/Car.java
+++ src/carRace/Car.java
@@ -1,20 +1,29 @@
 package carRace;
 
-public class Car {
-    private static int speedLimit;
+public class Car extends Vehicle {
+    private static int speedLimit = 110;
     private int normalSpeed;
-    private String name;
-    private int distanceTravelled;
 
-    public Car(){
-
+    public Car(String name, int normalSpeed) {
+        super();
+        this.name = name;
+        this.normalSpeed = normalSpeed;
     }
 
-    public static int setSpeedLimit(int limit) {
-
+    public static void setSpeedLimit(int limit) {
+        if (limit == 70) {
+            speedLimit = limit;
+        } else {
+            speedLimit = 110;
+        }
     }
 
+    @Override
     public void moveForAnHour() {
-
+        if (speedLimit == 70) {
+            distanceTravelled += speedLimit;
+        } else {
+            distanceTravelled += normalSpeed;
+        }
     }
 }
--- src/carRace/Main.java
+++ src/carRace/Main.java
@@ -1,21 +1,58 @@
 package carRace;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
     static boolean isRaining = false; // 30% every hour
+    static ArrayList<Vehicle> vehicles = new ArrayList<>(30);
 
     private static void createVehicles() {
+        String[] carNames = new String[]{
+                "Barrage",
+                "Celestial",
+                "Lightning",
+                "Guerilla",
+                "Umbra",
+                "Formula",
+                "Warrior",
+                "Adventure",
+                "Crusader",
+                "Paradox"
+        };
+        Random randomGen = new Random();
         // 10 cars, 10 motorcycles, 10 trucks
+        for (int i = 0; i < 10; i++) {
+            String newCarName = carNames[randomGen.nextInt(carNames.length)];
+            newCarName += " " + carNames[randomGen.nextInt(carNames.length)];
+            int newCarSpeedRandomness = randomGen.nextInt(31); // The difference between 110 and 80
+            vehicles.add(new Car(newCarName, 80 + newCarSpeedRandomness));
+        }
     }
 
     private static void simulateRace() {
-        // 50 iteration of weather setting and calling moveForAnHour on each vehicle
+        Random randomGenerator = new Random();
+        for (int i = 0; i < 50; i++) {
+            // Set variables for current hour
+            isRaining = (randomGenerator.nextInt(10) < 3) ? true : false;
+            int newCarSpeedLimit = (randomGenerator.nextInt(10) < 3) ? 70 : 110;
+            Car.setSpeedLimit(newCarSpeedLimit);
+            
+            for (Vehicle vehicle : vehicles) {
+                vehicle.moveForAnHour();
+            }
+        }
     }
 
     private static void printRaceResults() {
-        // print each vehicles name, type and distance travelled
+        for (Object vehicle : vehicles) {
+            System.out.println(vehicle + "\n");
+        }
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        createVehicles();
+        simulateRace();
+        printRaceResults();
     }
 }
